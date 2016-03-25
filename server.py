3f7d2899ef17e5a8bcc2370b0bd91b59047f88fc
Doshmajhan <cpc1007@g.rit.edu>
2016-03-25 20:44:12 +0000

    Can decode the header, working on decoding the question body

--- server.py
+++ server.py
@@ -12,35 +12,46 @@ print "Listening on port %d" % bind_port
 
 #Decode the header of the packet
 def decode_header(query):
-    myId, request = struct.unpack('!HH', query[:4])
-    qr = request >> 15
-    opcode = (request >> 11) & 0xf
-    aa = (request >> 10) % 0x1
-    tc = (request >> 9) % 0x1
-    rd = (request >> 8) % 0x1
-    ra = (request >> 7) % 0x1
-    z = (request >> 6) % 0x1
-    ad = (request >> 5) % 0x1
-    cd = (request >> 4) % 0x1
-    rcode = request & 0xf
+    queryId, flags, qCount, ansCount, authCount, addCount  = struct.unpack('!HHHHHH', query[:12])
+    qr = flags >> 15
+    opcode = (flags >> 11) & 0xf
+    aa = (flags >> 10) % 0x1
+    tc = (flags >> 9) % 0x1
+    rd = (flags >> 8) % 0x1
+    ra = (flags >> 7) % 0x1
+    z = (flags >> 6) % 0x1
+    ad = (flags >> 5) % 0x1
+    cd = (flags >> 4) % 0x1
+    rcode = flags & 0xf
     
-    print qr
-    print opcode
-    print aa
-    print tc
-    print rd
-    print ra
-    print z
-    print ad
-    print cd
-    print rcode
+    print "[QR %d] [OPC %d] [AA %d] [TC %d] [RD %d] [RA %d] [Z %d] [AD %d] [CD %d] [RCODE %d]" % \
+           (qr, opcode, aa, tc, rd, ra, z, ad, cd, rcode)
 
+    print "[# Questions: %d] [# Ans RR: %d] [# Auth RR: %d] [# Add RR: %d]" % \
+            (qCount, ansCount, authCount, addCount)
+
+    return qCount
+
+#Decode the variable length question body
+def decode_question (query, offset):
+    qFormat = struct.Struct("!HH")
+    qEntries = []
+    qCount = decode_header(query
+    while x < qCount:
+        qtype, qclass = qFormat.unpack_from(query_body, offset)
+        offset += 2
+        qEntries += [{"qType": qtype, "qClass": qclass}]
+        x+=1
+
+    return qEntries  
+    
 #Handle a DNS Query
 def handle_request(request):
 
     print "Recieved: %s" % request
     print "TRYING TO DECODE"
-    decode_header(request)
+    entries = decode_question(request, 12)
+    print entries
 
 if __name__ == "__main__":
     
