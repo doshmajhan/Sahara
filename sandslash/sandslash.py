c03ed88c21cd2e5f639bb9134eafa021f093f4de
Doshmajhan <cpc1007@g.rit.edu>
2016-03-27 23:29:04 +0000

    each class is in its own file, have main script now that runs interactive prompt and reads in commands from user

--- sandslash/sandslash.py
+++ sandslash/sandslash.py
@@ -6,11 +6,54 @@
     @author Dosh, JRoc
     github.com/doshmajhan/Sandshrew
 """
+import threading, sys
 import server
 
-# Main program to start the server and receive requests
+"""
+    Function to start interactive prompt to read commands
+
+"""
+def start_prompt():
+    port = 53   # Default value
+    cmd = ""
+
+    while True:
+        try:
+            cmd = raw_input("[*] ")
+        except (KeyboardInterrupt, SystemExit):
+            print ""
+            sys.exit()
+              
+        if cmd == "start":
+            s = start_server(port)
+
+        split_cmd = cmd.split(" ")
+        if split_cmd[0] == "port":
+            port = int(split_cmd[1])
+         
+"""
+    Function to handle the start command.
+    Starts server and returns the server object created
+    
+    port - the port to listen on 
+
+    returns a server object
+"""
+def start_server(port):
+    s = server.Server(port)
+    server_thread = threading.Thread(target=s.start)
+    server_thread.daemon=True
+    try:
+        server_thread.start()
+        print "Server listening on port %d" % port
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
+    return s
+
+"""
+    Main program to start the server and receive requests
+"""
 if __name__ == "__main__":
-        port = int(raw_input("Port: "))
-        s = server.Server(port)
-        s.start()
-        exit(0)
+        print "- Starting prompt..."
+        start_prompt()
+        sys.exit()
--- sandslash/server.py
+++ sandslash/server.py
@@ -3,7 +3,7 @@
     the DNS server and handle queries/repsonses
     @author Dosh, JRoc
 """
-import socket, threading
+import socket, threading, sys
 import query
 
 """
@@ -12,9 +12,10 @@ import query
     port - the port number to bind to
 """
 class Server:
-    def __int__(self, port):
+    def __init__(self, port):
         self.port= port
         self.sock = None
+    
 
     """
         Function to start the server with the information
@@ -25,10 +26,13 @@ class Server:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(('', self.port))
 
-        print "Listening on port %d" % self.port
-
         while True:
             #Accept query and start a new thread
             q, addr = self.sock.recvfrom(8192)
             query_handler = threading.Thread(target=query.handle_query, args=(q, addr, self.sock))
-            query_handler.start()
+            query_handler.daemon=True
+            try:
+                query_handler.start()
+            except (KeyboardInterrupt, SystemExit):
+                self.sock.close()
+                sys.exit()
